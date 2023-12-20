9cb5a99ca5d783635a2b517d8382846c6f4085ec
kimsunwoo1212 <tjsdn44220354@gmail.com>
2023-12-20 15:48:16 +0900

    1단계 종료기능 구현

--- src/App.java
+++ src/App.java
@@ -10,11 +10,11 @@ public class App {
 	public void run() {
 		System.out.println("== 명언 앱 실행 ==");
 		// trim() : 혹시 있을지 모르는 좌우 공백 제거
-		while(true) {
+		while (true) {
 			System.out.print("명령어 ) ");
 			String cmd = sc.nextLine().trim();
 
-			if(cmd.equals("종료")) {
+			if (cmd.equals("종료")) {
 				break;
 			}
 		}
