9683055806bcd3cff11eb86bcdffa314c38f2c33
kimsunwoo1212 <tjsdn44220354@gmail.com>
2023-12-20 16:22:56 +0900

    2단계 명언 입력받기

--- src/wise_saying_2023_12/App.java
+++ src/wise_saying_2023_12/App.java
@@ -1,3 +1,5 @@
+package wise_saying_2023_12;
+
 import java.util.Scanner;
 
 public class App {
@@ -9,13 +11,19 @@ public class App {
 
 	public void run() {
 		System.out.println("== 명언 앱 실행 ==");
-		// trim() : 혹시 있을지 모르는 좌우 공백 제거
+
 		while (true) {
 			System.out.print("명령어 ) ");
 			String cmd = sc.nextLine().trim();
 
 			if (cmd.equals("종료")) {
 				break;
+			} else if (cmd.equals("등록")) {
+				System.out.print("명언 : ");
+				String content = sc.nextLine().trim();
+				System.out.print("작가 : ");
+				String author = sc.nextLine().trim();
+				System.out.println("1번 명언이 등록되었습니다.");
 			}
 		}
 
--- src/wise_saying_2023_12/Main.java
+++ src/wise_saying_2023_12/Main.java
@@ -1,8 +1,9 @@
+package wise_saying_2023_12;
+
 import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("123123");
 
 		Scanner sc = new Scanner(System.in);
 
